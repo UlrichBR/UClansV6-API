8ca4ea93ddc6dd1420fb4a4aae7999b7e2bde21e
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-02-13 21:00:09 -0300

    Update LibAPI.java

--- src/main/java/me/ulrich/clans/interfaces/LibAPI.java
+++ src/main/java/me/ulrich/clans/interfaces/LibAPI.java
@@ -1,30 +1,11 @@
 package me.ulrich.clans.interfaces;
 
-import java.util.HashMap;
-
 import org.bukkit.Location;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import me.ulrich.clans.data.CommandData;
-import me.ulrich.clans.data.CommandData_actionbar;
-import me.ulrich.clans.data.CommandData_disable;
-import me.ulrich.clans.data.CommandData_show;
-import me.ulrich.clans.data.CommandData_sound;
-import me.ulrich.clans.data.CommandData_title;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 
 public interface LibAPI {
-
-	HashMap<String, CommandData> getClanCommander();
-
-	HashMap<String, CommandData> getUClanCommander();
-	
-	void unRegisterCommand(String command);
-
-	void insertCommand(String command, String permission, String alias, double value, int cooldown, CommandData_show data,CommandData_title title, CommandData_actionbar actionbar, CommandData_sound sound, HashMap<String, CommandData_show> subcommands, CommandData_disable disable);
-
-	void registerCommand(String alias, ClansCommand command);
 
 	void sendTitle(Player p, String msg, int fadeIn, int stay, int fadeOut);
 
@@ -41,24 +22,6 @@ public interface LibAPI {
 	Hologram createHolo(final Location location, String id, Object extra);
 
 	BukkitAudiences getAdventure();
-
-	HashMap<String, String> getCommandAliases();
-
-	HashMap<String, String> getMainCommandAliases();
-
-	String replaceMainCmd(String string);
-
-	String replaceCmd(String string);
-
-	void showHelpSubmessage(CommandSender sender, CommandData commander);
-
-	boolean findAliases(String sub, String alias);
-
-	String findReturnAliases(String sub);
-
-	String findReturnMain(String sub);
-
-	String findAliasesByValue(String sub);
 
 	boolean isUseGUI();
 	
