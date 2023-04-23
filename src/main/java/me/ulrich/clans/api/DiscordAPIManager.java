a1f989ff41a46df5794efdcd85d846f104eb59a5
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-04-23 15:17:53 -0300

    Update DiscordAPIManager.java

--- src/main/java/me/ulrich/clans/api/DiscordAPIManager.java
+++ src/main/java/me/ulrich/clans/api/DiscordAPIManager.java
@@ -8,34 +8,33 @@ import me.ulrich.clans.interfaces.DiscordAPI;
 
 public class DiscordAPIManager implements DiscordAPI {
 
-	private final Clans plugin;
 
 	public DiscordAPIManager(Clans clans) {
-		this.plugin = clans;
+
 	}
 	
 	@Override
 	public boolean sendClanHookMessage(UUID clanUUID, String title, String content, String autor, String autorIcon ) {
-		return this.plugin.getDiscordManager().sendClanHookMessage(clanUUID, title, content, autor, autorIcon);
+		return false;
 	}
 	
 	@Override 
 	public boolean hasWebhook(UUID clanUUID) {
-		return this.plugin.getDiscordManager().hasWebhook(clanUUID);
+		return false;
 	}
 	
 	@Override
 	public Optional<String> getWebhook(UUID clanUUID) {
-		return this.plugin.getDiscordManager().getWebhook(clanUUID);
+		return Optional.empty();
 	}
 	
 	@Override
 	public boolean changeWebhook(UUID clanUUID, String url) {
-		return this.plugin.getDiscordManager().changeWebhook(clanUUID, url);
+		return false;
 	}
 	
 	@Override
 	public boolean removeWebhook(UUID clanUUID) {
-		return this.plugin.getDiscordManager().removeWebhook(clanUUID);
+		return false;
 	}
 }
