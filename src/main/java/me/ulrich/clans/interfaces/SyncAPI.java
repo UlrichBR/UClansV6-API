fbe3be092a90df90546b65aeb03a38669d569935
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-05-08 17:34:04 -0300

    Create SyncAPI.java

---
+++ src/main/java/me/ulrich/clans/interfaces/SyncAPI.java
@@ -0,0 +1,21 @@
+package me.ulrich.clans.interfaces;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+import me.ulrich.clans.data.ClanEnum.RedisType;
+
+public interface SyncAPI {
+
+	boolean syncEnabled();
+
+	void newSync(RedisType type, String string);
+
+	void serverConnector(Player player, String server);
+
+	List<String> getProxyedPlayers();
+
+	void setProxyedPlayers(List<String> proxyedPlayers);
+
+}
